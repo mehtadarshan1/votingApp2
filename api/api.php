6bced59400f4289669455718002148198eef7d2c
mehtadarshan <darshanmehta786@yahoo.in>
2017-04-15 23:31:04 -0400

    s

--- api/api.php
+++ api/api.php
@@ -44,7 +44,7 @@ switch ($method) {
     if ($operation == "login"){
       $usernm=$_POST['username']; 
       $password=$_POST['password'];
-      echo $_POST;
+      echo json_encode($_POST);
       if(empty($usernm)||empty($password)){
         break;
       }
